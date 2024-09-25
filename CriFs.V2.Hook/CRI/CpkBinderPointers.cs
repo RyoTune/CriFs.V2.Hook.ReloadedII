3a43aa0f37ca4e140d40345f57b209747ee69d13
RyoTune <zutomayofan50@gmail.com>
2024-09-25 19:30:55 +0900

    Add Metaphor patterns.

--- CriFs.V2.Hook/CRI/CpkBinderPointers.cs
+++ CriFs.V2.Hook/CRI/CpkBinderPointers.cs
@@ -567,7 +567,43 @@ internal static class CpkBinderPointers
                         DisableFileBindWarning = "",
                         DisableGetContentsInfoDetailsWarning = "" // not supported
                     }
-                }
+                },
+                new()
+                {
+                    SourcedFrom = "Metaphor Re:Fantazio",
+                    CriVersion = "CRI File System/PCx64 Ver.2.85.1 Build:Oct  2 2023 10:26:25",
+                    CriCompiler = "MSC19.16.27045.0,MT",
+                    Patterns = new CriPointerPatterns
+                    {
+                        CriFs_CalculateWorkSizeForLibrary =
+                            "48 89 5C 24 ?? 48 89 74 24 ?? 55 57 41 54 41 56 41 57 48 8D 6C 24 ?? 48 81 EC A0 00 00 00 48 8B 05",
+                        CriFs_InitializeLibrary = "4C 8B DC 49 89 5B ?? 49 89 6B ?? 49 89 7B ?? 41 56 48 83 EC 60",
+                        CriFs_FinalizeLibrary = "48 83 EC 28 83 3D ?? ?? ?? ?? 00 75 ?? 48 8D 15 ?? ?? ?? ?? 33 C9 E8 ?? ?? ?? ?? 83 C8 FF E9",
+                        CriFsBinder_BindCpk = "48 83 EC 48 48 8B 44 24 ?? C7 44 24 ?? 01 00 00 00",
+                        CriFsBinder_BindFile =
+                            "48 83 EC 48 48 8B 44 24 78 48 89 44 24 30 8B 44 24 70 89 44 24 28 4C 89 4C 24 20 41 B9",
+                        CriFsBinder_BindFiles =
+                            "48 83 EC 48 48 8B 44 24 78 48 89 44 24 30 8B 44 24 70 89 44 24 28 4C 89 4C 24 20 41 83",
+                        CriFsBinder_Find =
+                            "48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 57 48 83 EC 40 49 8B F9 49 8B D8 48",
+                        CriFsBinder_GetSizeForBindFiles = "48 89 5C 24 08 48 89 74 24 20 57 48 81 EC 50",
+                        CriFsBinder_GetStatus = "48 89 5C 24 08 57 48 83 EC 20 48 8B DA 8B F9 85",
+                        CriFsBinder_SetPriority =
+                            "48 89 5C 24 08 57 48 83 EC 20 8B FA E8 ?? ?? ?? ?? 48 8B D8 48 85 C0 75 18",
+
+                        CriFsBinder_Unbind = "48 89 5C 24 ?? 57 48 83 EC 20 8B F9 E8 ?? ?? ?? ?? 48 8B D8",
+
+                        CriFsLoader_RegisterFile = "48 8B C4 48 89 58 ?? 48 89 70 ?? 4C 89 60",
+
+                        CriFsIo_Exists = "48 89 5C 24 18 57 48 81 EC 70 08",
+                        CriFsIo_Open =
+                            "48 8B C4 48 89 58 ?? 48 89 68 ?? 48 89 70 ?? 57 41 54 41 55 41 56 41 57 48 83 EC 50 4D 8B E9",
+                        CriFsIo_IsUtf8 =
+                            "83 3D ?? ?? ?? ?? ?? 74 38 E8 ?? ?? ?? ?? 48 8D 4C 24 30 C7 44 24 28 11 04 00 00 48 89 4C 24 20 4C 8B C7",
+                        DisableFileBindWarning = "E8 ?? ?? ?? ?? 83 C8 FF EB 45 48 8B 0D",
+                        DisableGetContentsInfoDetailsWarning = "" // not supported
+                    }
+                },
             };
         }
         else if (IntPtr.Size == 4)
